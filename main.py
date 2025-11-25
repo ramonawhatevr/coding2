b93c4e1f75696fca4ef815e200002cd3b1f25031
VicachuPika <vicaaimekoala@icloud.com>
2025-11-25 16:57:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-print("Welcome to the disgusting magical forest!")
-
-print("You are on a path, but it diverges at some point. What will you do?")
-
-input("left or right?")
-
-x = input()
-
-
-if x == "left":
-    print("you reached a swamp! it stinks")
-
-elif x == "right":
-    print("you reached a garden! bugs are everywhere")
-    
-if "right" == print("you reached a garden! bugs are everywhere"):
-    print("you see two doors to get into a castle! door one or two?")
-    
-elif "left" == print("you reached a swap! it stinks"):
-    print("you see two dirt paths in front! path one or two?")
-    
+def which_direction():
+  while True:
+  go = input("Which way do you want to go, left or right? ")
+    if go == "": continue
+  go = go[0].lower()
+  if go in ['l', 'r']:
+      return go
+    else:
+      print("I don't understand")  
+  return go
+player = input("What's your name explorer? ")
+print("Welcome to the Auror village,", player)
+print()
+print("You find yourself in a village")
+print("You can see two villagers")
+print("one attractive lady on the right, one old hag on the left")
+go = which_direction()
+if go == 'l':
+   print("You have chosen to speak to the old hag")
+if go == 'r':
+   print("You have chosen to speak to the attractive lady")
